fbcd1eabe8bcae00ccd236a5302936d00c0ba626
K-Kensuke <black1993raven@gmail.com>
2014-06-04 20:05:27 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#FileSearch
+Pythonを使ったファイルサーチ
+
+##Auther
+Kensuke Kousaka（@kensuke_linx）
+
+##License
+This software is released under the **MIT License**, see LICENSE.txt
---
+++ search.py
@@ -0,0 +1,18 @@
+#coding:utf-8
+"""
+Copyright (c) 2014. Kensuke Kousaka
+
+This software is released under the MIT License
+
+http://opensource.org/licenses/mit-license.php
+"""
+__author__ = 'Kensuke Kousaka'
+
+import sys
+import os
+
+def main(argv):
+    print 'TEST'
+
+if __name__ == '__main__':
+    main(sys.argv)
